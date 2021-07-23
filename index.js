ca7def1f11027eced5fd3dd56f78524a988c8e81
jwarhurst22 <jawarhurst@hotmail.com>
2021-07-22 21:56:28 -0700

    implemented logout ability

--- index.js
+++ index.js
@@ -66,6 +66,10 @@ app.get("/auth/github", passport.authenticate('github'));
 app.get("/auth/github/callback", passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {
   res.redirect('/reminders')
 });
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
 
 app.listen(3001, function () {
   console.log(
